a7decada8a82bc2d2d35c3e86493860d0a4c9853
Dev789-del <huynq.bi11-113@st.usth.edu.vn>
2024-01-03 15:37:10 +0700

    Publish file

---
+++ model/data_info.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+#Describe train csv data
+def describe_train_csv():
+    #Load data from train.csv
+    train_data = pd.read_csv('./model/train.csv')
+    train_data = train_data.sample(frac=1).reset_index(drop=True)
+    train_data.head()
+
+    #Describe the csv file
+    print(train_data.describe())
+    print(train_data.info())
+
+#Describe test csv data
+def describe_test_csv():
+    #Load data from test.csv
+    test_data = pd.read_csv('./model/test.csv')
+    test_data = test_data.sample(frac=1).reset_index(drop=True)
+    test_data.head()
+
+    #Describe the csv file
+    print(test_data.describe())
+    print(test_data.info())
+
+print('Train data:')
+describe_train_csv()
+print('Test data:')
+describe_test_csv()
